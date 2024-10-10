rainmore/spring-react-demo

introduced pagination

ui/src/components/layout/pagination-component.tsx
@@ -1,0 +1,68 @@
+import React from 'react'
+import { Page } from '../../services/api/types'
+import { PageService } from '../../services/page-service'
+
+interface Props {
+  page: Page<Any>
+}
+
+const PaginationComponent: React.FC<Props> = ({ page }: Props) => {
+  const pageService = new PageService()
+
+  return (
+    !page.empty && (
+      <nav className="pagination is-centered" role="navigation" aria-label="pagination">
+        {page.first ? (
+          <>
+            <a className="pagination-previous is-disabled">&lt;&lt;</a>
+            <a className="pagination-previous is-disabled">&lt;</a>
+          </>
+        ) : (
+          <>
+            <a href={pageService.firstPagePath()} className="pagination-previous">
+              &lt;&lt;
+            </a>
+            <a href={pageService.prevPagePath(page)} className="pagination-previous">
+              &lt;
+            </a>
+          </>
+        )}
+        {page.last ? (
+          <>
+            <a className="pagination-next is-disabled">&gt;</a>
+            <a className="pagination-next is-disabled">&gt;&gt;</a>
+          </>
+        ) : (
+          <>
+            <a href={pageService.nextPagePath(page)} className="pagination-next">
+              &gt;
+            </a>
+            <a href={pageService.lastPagePath(page)} className="pagination-next">
+              &gt;&gt;
+            </a>
+          </>
+        )}
+        <ul className="pagination-list">
+          {pageService.getPaginationRage(page).map((number, idx) => {
+            const displayNumber = number + 1
+            return (
+              <li key={`pagination-page-item-${number}-${idx}`}>
+                {number == page.number ? (
+                  <a className="pagination-link is-current" aria-label={`Page ${displayNumber}`} aria-current="page">
+                    {displayNumber}
+                  </a>
+                ) : (
+                  <a href={pageService.buildPath(number)} className="pagination-link" aria-label={`Goto page ${displayNumber}`}>
+                    {displayNumber}
+                  </a>
+                )}
+              </li>
+            )
+          })}
+        </ul>
+      </nav>
+    )
+  )
+}
+
+export default PaginationComponent

ui/src/pages/book/list-page.tsx
@@ -1,20 +1,22 @@
 import React, { useEffect, useState } from 'react'
-import { bookService } from '../../services/book/book-service'
-import { toastService } from '../../services/toast-service'
-import { Page, Pageable } from '../../services/api/types'
-import { Book } from '../../services/book/types'
-import { Axios, CancelToken } from 'axios'
 import LoadingComponent from '../../components/layout/loading-component'
+import PaginationComponent from '../../components/layout/pagination-component'
+import { Page } from '../../services/api/types'
+import { bookService } from '../../services/book/book-service'
+import { Book } from '../../services/book/types'
+import { PageService } from '../../services/page-service'
+import { toastService } from '../../services/toast-service'
 
 const ListPage: React.FC = () => {
   const [loading, setLoading] = useState(true)
 
-  const [pageable, setPageable] = useState<Pageable | null>(null)
+  const pageService = new PageService()
+
   const [pageData, setPageDate] = useState<Page<Book> | null>(null)
 
   useEffect(() => {
     bookService
-      .find(pageable || undefined)
+      .find(pageService.getPageable())
       .then((data) => {
         setPageDate(data)
         setLoading(false)
@@ -26,38 +28,39 @@ const ListPage: React.FC = () => {
 
   return (
     <>
-      <div className="block">
-        <h1 className="title">Books</h1>
-      </div>
+      <h1 className="title">Books</h1>
       <div className="box">
         {loading ? (
           <LoadingComponent />
         ) : (
-          <table className="table is-striped is-hoverable">
-            <thead>
-              <tr>
-                <th>#</th>
-                <th>Name</th>
-                <th>Category</th>
-                <th>Publish Date</th>
-                <th></th>
-              </tr>
-            </thead>
-            <tbody>
-              {pageData &&
-                pageData.content.map((row, idx) => {
-                  return (
-                    <tr key={`row-book-${row.id}-${idx}`}>
-                      <td>{row.id}</td>
-                      <td>{row.name}</td>
-                      <td>{row.category?.name}</td>
-                      <td>{row.publicationDate}</td>
-                      <td></td>
-                    </tr>
-                  )
-                })}
-            </tbody>
-          </table>
+          <>
+            <table className="table is-fullwidth is-striped is-hoverable">
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>Name</th>
+                  <th>Category</th>
+                  <th>Publish Date</th>
+                  <th></th>
+                </tr>
+              </thead>
+              <tbody>
+                {pageData &&
+                  pageData.content.map((row, idx) => {
+                    return (
+                      <tr key={`row-book-${row.id}-${idx}`}>
+                        <td>{row.id}</td>
+                        <td>{row.name}</td>
+                        <td>{row.category?.name}</td>
+                        <td>{row.publicationDate}</td>
+                        <td></td>
+                      </tr>
+                    )
+                  })}
+              </tbody>
+            </table>
+            <PaginationComponent page={pageData} />
+          </>
         )}
       </div>
     </>

rainmore/reactjs-book-store-example

src/components/pagination/pagination-component.tsx
@@ -1,0 +1,68 @@
+import React from 'react'
+import { Page } from '../../services/api/types'
+import { PaginationService } from '../../services/pagination-service'
+
+interface Props {
+  page: Page<Any>
+}
+
+const PaginationComponent: React.FC<Props> = ({ page }: Props) => {
+  const paginationService = new PaginationService()
+
+  return (
+    !page.empty && (
+      <nav className="pagination is-centered" role="navigation" aria-label="pagination">
+        {page.first ? (
+          <>
+            <a className="pagination-previous is-disabled">&lt;&lt;</a>
+            <a className="pagination-previous is-disabled">&lt;</a>
+          </>
+        ) : (
+          <>
+            <a href={paginationService.firstPagePath()} className="pagination-previous">
+              &lt;&lt;
+            </a>
+            <a href={paginationService.prevPagePath(page)} className="pagination-previous">
+              &lt;
+            </a>
+          </>
+        )}
+        {page.last ? (
+          <>
+            <a className="pagination-next is-disabled">&gt;</a>
+            <a className="pagination-next is-disabled">&gt;&gt;</a>
+          </>
+        ) : (
+          <>
+            <a href={paginationService.nextPagePath(page)} className="pagination-next">
+              &gt;
+            </a>
+            <a href={paginationService.lastPagePath(page)} className="pagination-next">
+              &gt;&gt;
+            </a>
+          </>
+        )}
+        <ul className="pagination-list">
+          {paginationService.getPaginationRage(page).map((number, idx) => {
+            const displayNumber = number + 1
+            return (
+              <li key={`pagination-page-item-${number}-${idx}`}>
+                {number == page.number ? (
+                  <a className="pagination-link is-current" aria-label={`Page ${displayNumber}`} aria-current="page">
+                    {displayNumber}
+                  </a>
+                ) : (
+                  <a href={paginationService.buildPath(number)} className="pagination-link" aria-label={`Goto page ${displayNumber}`}>
+                    {displayNumber}
+                  </a>
+                )}
+              </li>
+            )
+          })}
+        </ul>
+      </nav>
+    )
+  )
+}
+
+export default PaginationComponent

src/pages/book/list-page.tsx
@@ -1,20 +1,22 @@
 import React, { useEffect, useState } from 'react'
-import { bookService } from '../../services/book/book-service'
-import { toastService } from '../../services/toast-service'
-import { Page, Pageable } from '../../services/api/types'
-import { Book } from '../../services/book/types'
-import { Axios, CancelToken } from 'axios'
 import LoadingComponent from '../../components/layout/loading-component'
+import PaginationComponent from '../../components/pagination/pagination-component'
+import { Page } from '../../services/api/types'
+import { bookService } from '../../services/book/book-service'
+import { Book } from '../../services/book/types'
+import { PaginationService } from '../../services/pagination-service'
+import { toastService } from '../../services/toast-service'
 
 const ListPage: React.FC = () => {
   const [loading, setLoading] = useState(true)
 
-  const [pageable, setPageable] = useState<Pageable | null>(null)
+  const paginationService = new PaginationService()
+
   const [pageData, setPageDate] = useState<Page<Book> | null>(null)
 
   useEffect(() => {
     bookService
-      .find(pageable || undefined)
+      .find(paginationService.getPageable())
       .then((data) => {
         setPageDate(data)
         setLoading(false)
@@ -26,38 +28,39 @@ const ListPage: React.FC = () => {
 
   return (
     <>
-      <div className="block">
-        <h1 className="title">Books</h1>
-      </div>
+      <h1 className="title">Books</h1>
       <div className="box">
         {loading ? (
           <LoadingComponent />
         ) : (
-          <table className="table is-striped is-hoverable">
-            <thead>
-              <tr>
-                <th>#</th>
-                <th>Name</th>
-                <th>Category</th>
-                <th>Publish Date</th>
-                <th></th>
-              </tr>
-            </thead>
-            <tbody>
-              {pageData &&
-                pageData.content.map((row, idx) => {
-                  return (
-                    <tr key={`row-book-${row.id}-${idx}`}>
-                      <td>{row.id}</td>
-                      <td>{row.name}</td>
-                      <td>{row.category?.name}</td>
-                      <td>{row.publicationDate}</td>
-                      <td></td>
-                    </tr>
-                  )
-                })}
-            </tbody>
-          </table>
+          <>
+            <table className="table is-fullwidth is-striped is-hoverable">
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>Name</th>
+                  <th>Category</th>
+                  <th>Publish Date</th>
+                  <th></th>
+                </tr>
+              </thead>
+              <tbody>
+                {pageData &&
+                  pageData.content.map((row, idx) => {
+                    return (
+                      <tr key={`row-book-${row.id}-${idx}`}>
+                        <td>{row.id}</td>
+                        <td>{row.name}</td>
+                        <td>{row.category?.name}</td>
+                        <td>{row.publicationDate}</td>
+                        <td></td>
+                      </tr>
+                    )
+                  })}
+              </tbody>
+            </table>
+            <PaginationComponent page={pageData} />
+          </>
         )}
       </div>
     </>